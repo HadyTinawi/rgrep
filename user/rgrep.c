4558df088a95ec44e298a255507f90f1338518ca
Carlos Crespo <ccrespo3@gojira.cs.usfca.edu>
2025-09-21 00:39:25 -0700

    setup?

---
+++ user/rgrep.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char buf[1024]
+int match(char*, char*)
+
+void rgrep(char *pattern, char *path, int flg)
+{
+	
+	
+}
